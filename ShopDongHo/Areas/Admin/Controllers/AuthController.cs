284e19a109b3da4034889d476a74ae8239c236c9
Ciel <23DH110200@st.huflit.edu.vn>
2024-11-07 11:31:36 +0700

    1

--- ShopDongHo/Areas/Admin/Controllers/AuthController.cs
+++ ShopDongHo/Areas/Admin/Controllers/AuthController.cs
@@ -6,31 +6,12 @@ namespace ShopDongHo.Areas.Admin.Controllers
 {
     public class AuthController : Controller
     {
-       
-        protected override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
-            if (Session["MaNhanVien"] == null)
-            {
-                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { action = "Login", controller = "Home" }));
-            }
-            else
-            {
-                if (Session["Quyen"].ToString().ToLower() == "false")
-                {
-                    if (controllerName != "KhachHang" && controllerName != "DongHo" && controllerName != "DatHang" && controllerName != "DatHang_ChiTiet")
-                    {
-                        filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { action = "Unauthorized", controller = "Home" }));
-                    }
-                }
-            }
-            base.OnActionExecuting(filterContext);
-        }
+
 
         protected void SetAlert(string message, string type)
         {
             TempData["AlertMessage"] = message;
-            if(type=="success")
+            if (type == "success")
             {
                 TempData["AlertType"] = "alert-success";
             }
